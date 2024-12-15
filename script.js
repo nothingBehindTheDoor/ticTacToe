931ba4e099cdbd943e48210b099dbbd22211975b
mudakakk <161918354+mudakakk@users.noreply.github.com>
2024-12-15 10:33:58 +0000

    finished styling and wrote more logic to work with the ui

--- script.js
+++ script.js
@@ -14,23 +14,44 @@ const gameBoard = (function () {
 
     const board = createBoard();
 
-    const printBoard = function () {
+    const printBoardToConsole = function () {
         console.log(board);
     };
 
-    function dropToken(player, row, column) {
+    const printBoard = function (e) {
+        if (game.won === false) {
+            e.target.textContent = game.getActivePlayer().marker;
+        }
+    }
 
+    function dropToken(player, row, column) {
         board[row][column] = player.marker;
     }
 
-    return {board, printBoard, dropToken};
+    const cells = document.querySelector('#game-container-inner').childNodes;
+
+    cells.forEach((cell) => {
+        cell.addEventListener('click', (e) => {
+            if (e.target.textContent !== '' || game.won === true) {
+                return;
+            } else {
+                printBoard(e);
+                game.makeMove(e.target.getAttribute('row'), e.target.getAttribute('column'));
+            }
+        })
+    })
+
+    return {board, printBoardToConsole, dropToken, cells};
 })();
 
 const game = (function() {
+    const gameFieldTitleBar = document.querySelector('#turn-info');
+    const statDisplay = document.querySelector('#stats');
+
     function playerFactory (name1 = 'playerOne', name2 = 'playerTwo') {
         const players = [
-            {name: name1, marker: 'X'},
-            {name: name2, marker: 'O'},
+            {name: name1, marker: 'X', winCount: 0},
+            {name: name2, marker: 'O', winCount: 0},
         ]
         
         return players;
@@ -42,36 +63,61 @@ const game = (function() {
 
     const getActivePlayer = () => activePlayer;
 
-
-    const makeMove = function (row, column) {
-        if (gameBoard.board[row][column] === '') {
-            gameBoard.dropToken(activePlayer, row, column);
-            if (checkGameStatus()) won = true;
-            activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
-            gameBoard.printBoard();
-        } else {
-            console.log('That cell is already occupied!');
-        }
-    }
-
     let won = false;
 
     let tie = false;
 
+    const makeMove = function (row, column) {
+        if (won === true) {
+            return;
+        } else if (checkTieStatus()) {
+            gameFieldTitleBar.textContent = (`it's a tie!`);
+            return;
+        }
+        if (gameBoard.board[row][column] === '' && won === false) {
+            gameBoard.dropToken(activePlayer, row, column);
+            if (checkGameStatus()) {
+                won = true;
+                activePlayer.winCount++;
+                activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
+
+                statDisplay.textContent = `X : ${players[0].winCount} - O : ${players[1].winCount}`;
+
+            } else if (won === false) {
+            activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
+            gameFieldTitleBar.textContent = `${activePlayer.marker}'s turn!`;
+            gameBoard.printBoardToConsole();
+        }} else {
+            console.log('That cell is already occupied!');
+        }
+    }
+
     const checkGameStatus = function () {
         for (let i = 0; i < 3; i++) {
             if (gameBoard.board[i][0] === gameBoard.board[i][1] && gameBoard.board[i][1] === gameBoard.board[i][2] && gameBoard.board[i][0] !== '') {
-                console.log(`${getActivePlayer().marker} won!`);
+                gameFieldTitleBar.textContent = (`${getActivePlayer().marker} won!`);
+                return true;
             } else if (gameBoard.board[0][i] === gameBoard.board[1][i] && gameBoard.board[1][i] === gameBoard.board[2][i] && gameBoard.board[0][i] !== '') {
-                console.log(`${getActivePlayer().marker} won!`);
+                gameFieldTitleBar.textContent = (`${getActivePlayer().marker} won!`);
+                return true;
             }
         }
 
         if (gameBoard.board[0][0] === gameBoard.board[1][1] && gameBoard.board[1][1] === gameBoard.board[2][2] && gameBoard.board[0][0] !== '') {
-            console.log(`${getActivePlayer().marker} won!`);
+            gameFieldTitleBar.textContent = (`${getActivePlayer().marker} won!`);
+            return true;
         } else if (gameBoard.board[0][2] === gameBoard.board[1][1] && gameBoard.board[1][1] === gameBoard.board[2][0] && gameBoard.board[0][2] !== '') {
-            console.log(`${getActivePlayer().marker} won!`);
+            gameFieldTitleBar.textContent = (`${getActivePlayer().marker} won!`);
+            return true;
         }
+    }
+
+    const checkTieStatus = function () {
+        let tie = 0;
+        for (const row of gameBoard.board) {
+            (row[0] !== '' && row[1] !== '' && row[2] !== '') ? tie += 1 : tie = tie;
+        }
+        return (tie === 2) ? true : false;
     }
 
     const restartGame = function () {
@@ -81,9 +127,19 @@ const game = (function() {
             }
         }
 
+        won = false;
         activePlayer = players[0];
+
+        gameFieldTitleBar.textContent = `${activePlayer.marker}'s turn!`;
+
+        gameBoard.cells.forEach((cell) => {
+            cell.textContent = '';
+        })
     }
 
-
-    return {players, getActivePlayer, makeMove, restartGame};
+    return {getActivePlayer, makeMove, restartGame, won};
 })();
+
+document.querySelector('#reset-btn').addEventListener('click', () => {
+    game.restartGame()
+});
